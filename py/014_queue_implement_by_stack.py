888d6d5c147c790db2878ce172670e74f8749ccf
Jianan Wang <jnwangcn@gmail.com>
2024-11-22 19:41:11 +0800

    update 014

--- py/014_queue_implement_by_stack.py
+++ py/014_queue_implement_by_stack.py
@@ -1,6 +1,6 @@
 class MyQueue:
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.in_stack: list[int] = []
         self.out_stack: list[int] = []
 
@@ -8,19 +8,21 @@ class MyQueue:
         self.in_stack.append(x)
 
     def pop(self) -> int:
-        if not self.out_stack:
-            for i in self.in_stack[::-1]:
-                self.out_stack.append(i)
-            self.in_stack = []
+        self.try_transfer()
         return self.out_stack.pop()
 
     def peek(self) -> int:
-        if not self.out_stack:
-            for i in self.in_stack[::-1]:
-                self.out_stack.append(i)
-            self.in_stack = []
+        self.try_transfer()
         return self.out_stack[-1]
 
     def empty(self) -> bool:
         return ((not self.in_stack) and (not self.out_stack))
+
+    def try_transfer(self) -> None:
+        if not self.out_stack:
+            for i in self.in_stack[::-1]:
+                self.out_stack.append(i)
+            self.in_stack = []
+        return
+
 
